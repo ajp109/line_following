e3c9a0cd0d2be56c3f1dd8faefa10660efb8906d
ajp109 <andrew.pomfret@york.ac.uk>
2023-07-27 14:51:38 +0000

    Change default speed, add help text

--- robot.cpp
+++ robot.cpp
@@ -1,6 +1,12 @@
 #include "robot.h"
 
 void control(Line const & line, Robot &robot) {
-    robot.speed = 0.3;      // metres per second, between 0 and 1
+    /* Available information:
+        line.position
+            The position of the line relative to the robot, arbitrary units, between -1 and +1
+        line.speed
+            The rate of change of the line position with respect to time
+    */
+    robot.speed = 0.1;      // metres per second, between 0 and 1
     robot.turnspeed = 0;    // radians per second, between -25 and +25 ish
 }
