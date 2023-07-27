d365553f0bda1ea5a8b23660206d7b79c316fdd4
ajp109 <andrew.pomfret@york.ac.uk>
2023-07-27 15:46:14 +0000

    Clamp wheel speeds

--- main.cpp
+++ main.cpp
@@ -4,22 +4,30 @@
 
 m3pi platform;
 
-float dt = 0.005;
+static const float r = 0.041;
+static const float dt = 0.005;
+
+static inline float clamp(float val, float maxval) {
+    if (val>=0) {
+        return (val>maxval) ? maxval : val;
+    }
+    return -clamp(-val, maxval);
+}
 
 static void harness(void) {
     static Robot robot;
     static Line line;
     static float e = platform.line_position();
-    static const float r = 0.041;
     float new_e = platform.line_position();
     float de_dt = (new_e - e) / dt;
     e = new_e;
-    float speed = 0.0, turn = 0.0;
     line.position = e;
     line.speed = de_dt;
     control(line, robot);
-    platform.left_motor(robot.speed+r*robot.turnspeed);
-    platform.right_motor(robot.speed-r*robot.turnspeed);
+    float delta = clamp(r*robot.turnspeed, 1.0f);
+    float speed = clamp(robot.speed, 1.0f-fabs(delta));
+    platform.left_motor(speed+delta);
+    platform.right_motor(speed-delta);
 }
 
 int main() {
